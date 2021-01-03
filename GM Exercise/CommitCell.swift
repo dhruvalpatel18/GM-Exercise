1f9ff1bbe37db7ccb61e8a99bd799db877327d61
Dhruval Patel <dhruvalpatel@tamrd.com>
2021-01-02 20:54:40 -0500

    Add CommitCell class and connected UI

---
+++ GM Exercise/CommitCell.swift
@@ -0,0 +1,17 @@
+//
+//  CommitCell.swift
+//  GM Exercise
+//
+//  Created by Dhruval Patel on 1/2/21.
+//
+
+import Foundation
+import UIKit
+
+class CommitCell: UITableViewCell {
+    
+    @IBOutlet weak var authorName: UILabel!
+    @IBOutlet weak var commitHash: UILabel!
+    @IBOutlet weak var commitMessage: UILabel!
+    
+}
