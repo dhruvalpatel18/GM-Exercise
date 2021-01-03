22127955d6d453f2b4c285df4d9daae1394db31b
Dhruval Patel <dhruvalpatel@tamrd.com>
2021-01-02 21:26:58 -0500

    Add tableview controller's  methods

--- GM Exercise/ViewController.swift
+++ GM Exercise/ViewController.swift
@@ -16,7 +16,20 @@ class ViewController: UITableViewController {
             print(String(data: data, encoding: .utf8)!)
         })
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommitCell", for: indexPath) as! CommitCell
+        cell.authorName.text = "Name \(indexPath.row)"
+        cell.commitHash.text = "Hash \(indexPath.row)"
+        cell.commitMessage.text = "Message \(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
