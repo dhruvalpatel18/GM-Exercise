b77b28628b0816cc22af3a75a328d8aec011d990
Dhruval Patel <dhruvalpatel@tamrd.com>
2021-01-03 13:33:39 -0500

    Add testcase for checkForReachability method which pretty much calls all the helper methods

--- GM ExerciseTests/GM_ExerciseTests.swift
+++ GM ExerciseTests/GM_ExerciseTests.swift
@@ -9,14 +9,44 @@ import XCTest
 @testable import GM_Exercise
 
 class GM_ExerciseTests: XCTestCase {
-
+    
+    var vcHelper: VCHelper!
+    var commitList: [CommitModel]!
+    
     override func setUpWithError() throws {
+        vcHelper = VCHelper()
+        commitList = [CommitModel]()
     }
 
     override func tearDownWithError() throws {
+        vcHelper = nil
+        commitList = nil
     }
 
-    func testExample() throws {
+    func testCheckForReachability() throws {
+        
+        let expectation = self.expectation(description: "Commit List With Updated Data")
+        
+        vcHelper.checkForReachability { [self] (isIntAvailable) in
+            
+            if isIntAvailable {
+                
+                commitList = vcHelper.commitList
+                XCTAssertGreaterThan(commitList.count, 0, "CommitList is empty")
+                XCTAssertNotEqual(commitList[0].author, "", "Error in parsing author name")
+                XCTAssertNotEqual(commitList[0].commitSha, "", "Error in parsing commit hash")
+                XCTAssertNotEqual(commitList[0].commitMessage, "", "Error in parsing commit message")
+                
+                expectation.fulfill()
+                
+            } else {
+                
+                XCTFail("No Internet Connection")
+                
+            }
+        }
+        
+        waitForExpectations(timeout: 5)
     }
 
     func testPerformanceExample() throws {
